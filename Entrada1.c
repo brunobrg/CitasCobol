406cfdee18a3c52d732a5bf0af2c92987d128296
ShutUpPaulo <markes.calado@gmail.com>
2015-07-01 05:25:33 -0300

    Criando testes para a apresentação

---
+++ Entrada1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{	
+	printf("Teste de reconhecimento de variaveis e de print grande o suficiente para quebrar a linha. \n");
+	int b = 20;
+	float c = 34.5;
+	int d = c;
+	char T= 't';
+}
